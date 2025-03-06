9f3aca57a7d4d78db3fa754c7910f206bf2fdb97
f5song <folkkykytak@gmail.com>
2025-03-06 21:09:32 +0700

    update register

--- frontend/src/pages/SingupPage.tsx
+++ frontend/src/pages/SingupPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "@/components/auth/AuthContext";
 import {
   useTermsPrivacy,
   TermsPrivacyProvider,
