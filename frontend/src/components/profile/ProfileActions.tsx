66440732108acab5a7558d658abaf9e3e3e83402
f5song <folkkykytak@gmail.com>
2025-03-07 14:38:30 +0700

    update bugs

--- frontend/src/components/profile/ProfileActions.tsx
+++ frontend/src/components/profile/ProfileActions.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { Profile } from "./ProfileTypes";
 
-interface ProfileActionsProps {
-  profile: Profile;
-}
-
-const ProfileActions: React.FC<ProfileActionsProps> = ({
-  profile
-}) => {
-  // Component no longer has any functionality since editing is removed
-  return null;
+const ProfileActions: React.FC = () => {
+  return null; // Component ไม่มี UI หรือฟังก์ชันการทำงาน
 };
 
 export default ProfileActions;
